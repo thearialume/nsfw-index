42fbdd409e17130dc484e93630b2e524a5dfdc2d
Aria Lume <thearialume@gmail.com>
2026-01-08 12:21:13 +0500

    Add DATABASE_URL to settings

--- nsfw_index/settings.py
+++ nsfw_index/settings.py
@@ -15,6 +15,12 @@ if USER_AGENT is None:
         "CRAWLER_USER_AGENT is not set in environment variables: Crawl responsibly by identifying yourself"
     )
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise Exception(
+        "DATABASE_URL is not set in environment variables: Where will you store data?"
+    )
+
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
